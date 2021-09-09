5da6eee79bc93503a3dfc1e32e17f6d60cc532a6
jpandiscia1709 <jpangithub1709@gmail.com>
2021-09-09 12:51:06 -0400

    created properties for robot class

--- Robot.py
+++ Robot.py
@@ -1,11 +1,20 @@
+from Weapon import Weapon
+
+
 class Robot:
-    def __init__(self):
-        self.name = ""
+    def __init__(self, name):
+        self.name = name
         self.health = 0
         self.weapon = Weapon
+        self.add_weapon_to_robot()
 
     def __init__(self, name):
         pass
+
+    def add_weapon_to_robot(self):
+        weapon1 = Weapon()
+
+        self.weapon.append(weapon1)
 
     def attack(self, dinosaur):
         pass
