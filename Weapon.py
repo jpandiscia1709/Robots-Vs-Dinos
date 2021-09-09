5869cd05e2e4cd980808a6f24c70c7eb1da57d79
jpandiscia1709 <jpangithub1709@gmail.com>
2021-09-09 12:26:18 -0400

    imported classes to link per UML

--- Weapon.py
+++ Weapon.py
@@ -1,6 +1,6 @@
 class Weapon:
-    def __init__(self):
-        self.name = ""
+    def __init__(self, weapon):
+        self.name = weapon
         self.attack_power = 0
 
     def __init__(self, name, attack_power):
